983420de6bda054dfb78c22f1e00310e00019c23
Swetha0407 <292122072+Swetha0407@users.noreply.github.com>
2026-08-05 10:38:01 +0530

    html tag  lex program

--- lex.yy.c
+++ lex.yy.c
@@ -282,23 +282,22 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
-static yyconst short int yy_accept[13] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[9] =
     {   0,
-        0,    0,    6,    4,    3,    3,    4,    2,    3,    1,
-        2,    0
+        0,    0,    4,    2,    2,    0,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    4,    1,    1,    5,    5,    5,
-        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
+        1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,33 +321,31 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[6] =
+static yyconst int yy_meta[5] =
     {   0,
-        1,    2,    2,    1,    3
+        1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[16] =
+static yyconst short int yy_base[10] =
     {   0,
-        0,    0,    9,   10,    0,    0,    0,    0,    0,    0,
-        0,   10,    6,    4,    3
+        0,    0,    8,    9,    3,    2,    9,    9,    4
     } ;
 
-static yyconst short int yy_def[16] =
+static yyconst short int yy_def[10] =
     {   0,
-       12,    1,   12,   12,   13,   13,   14,   15,   13,   14,
-       15,    0,   12,   12,   12
+        8,    1,    8,    8,    9,    9,    8,    0,    8
     } ;
 
-static yyconst short int yy_nxt[16] =
+static yyconst short int yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,   11,   10,    9,   12,    3,
-       12,   12,   12,   12,   12
+        4,    4,    5,    4,    6,    7,    7,    8,    3,    8,
+        8,    8,    8
     } ;
 
-static yyconst short int yy_chk[16] =
+static yyconst short int yy_chk[14] =
     {   0,
-        1,    1,    1,    1,    1,   15,   14,   13,    3,   12,
-       12,   12,   12,   12,   12
+        1,    1,    1,    1,    9,    6,    5,    3,    8,    8,
+        8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -362,13 +359,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "posnneg.l"
+#line 1 "html.l"
 #define INITIAL 0
-#line 2 "posnneg.l"
+#line 2 "html.l"
 #include<stdio.h>
-
-int positive_no = 0, negative_no = 0;
-#line 372 "lex.yy.c"
+#line 367 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -519,10 +514,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 7 "posnneg.l"
+#line 5 "html.l"
 
 
-#line 526 "lex.yy.c"
+#line 521 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -573,13 +568,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 13 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 10 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -607,36 +602,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 9 "posnneg.l"
-{
-                negative_no++;
-                printf("Negative number = %s\n", yytext);
-           }
+#line 7 "html.l"
+printf("%s\n", yytext);
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 14 "posnneg.l"
-{
-                positive_no++;
-                printf("Positive number = %s\n", yytext);
-           }
+#line 9 "html.l"
+;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 19 "posnneg.l"
-;
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 21 "posnneg.l"
-;
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 23 "posnneg.l"
+#line 11 "html.l"
 ECHO;
 	YY_BREAK
-#line 640 "lex.yy.c"
+#line 619 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -928,7 +907,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 13 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -963,11 +942,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 13 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 12);
+	yy_is_jam = (yy_current_state == 8);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1522,7 +1501,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 23 "posnneg.l"
+#line 11 "html.l"
 
 
 int yywrap()
@@ -1532,11 +1511,7 @@ int yywrap()
 
 int main()
 {
-    printf("Enter the numbers:\n");
+    printf("Enter HTML code:\n");
     yylex();
-
-    printf("\nPositive Count = %d\n", positive_no);
-    printf("Negative Count = %d\n", negative_no);
-
     return 0;
 }
