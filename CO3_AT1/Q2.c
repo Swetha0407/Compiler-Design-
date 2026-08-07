4919af4f605d18c70d898306c54698c6effb255a
Swetha0407 <292122072+Swetha0407@users.noreply.github.com>
2026-08-07 21:23:04 +0530

    CO3_AT1

---
+++ CO3_AT1/Q2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int stack[100];
+int top = -1;
+
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    char postfix[100];
+    int i, op1, op2;
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+
+    for(i = 0; postfix[i] != '\0'; i++)
+    {
+        if(isdigit(postfix[i]))
+        {
+            push(postfix[i] - '0');
+        }
+        else
+        {
+            op2 = pop();
+            op1 = pop();
+
+            switch(postfix[i])
+            {
+                case '+': push(op1 + op2); break;
+                case '-': push(op1 - op2); break;
+                case '*': push(op1 * op2); break;
+                case '/': push(op1 / op2); break;
+            }
+        }
+    }
+
+    printf("Result = %d", pop());
+
+    return 0;
+}
